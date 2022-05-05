4798f768b891248d892d5e1e0294fc16cd21636f
HAREESH BHITTAM <72018706+Hareesh108@users.noreply.github.com>
2022-05-05 16:33:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Developer", "YouTuber", "Designer", "Photographer","Developer"],
+        strings: [ "Developer", "Designer", "Photographer","Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "YouTuber", "Designer", "Photographer","Developer"],
+        strings: ["Developer", "Designer", "Photographer","Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
